1d0d9b3f027feb380b517e6f86dfbc59ff167942
Abdou_Bentegar <s_bentegar@enst.dz>
2022-04-24 21:32:53 +0100

    added global dependency for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,20 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
+import { babel } from '@rollup/plugin-babel';
 
 const packageJson = require("./package.json");
+
+const GLOBAL_DEPENDENCIES = {
+  'react': 'React',
+  'react-dom': 'ReactDOM',
+};
+
+const RESOLVE_PLUGIN_OPTIONS = {
+  extensions: ['.js']
+};
+
+
 
 export default {
   input: "src/index.ts",
@@ -12,12 +24,14 @@ export default {
     {
       file: packageJson.main,
       format: "cjs",
-      sourcemap: true
+      sourcemap: true,
+      globals: GLOBAL_DEPENDENCIES,
     },
     {
       file: packageJson.module,
       format: "esm",
-      sourcemap: true
+      sourcemap: true,
+      globals: GLOBAL_DEPENDENCIES,
     }
   ],
   plugins: [
